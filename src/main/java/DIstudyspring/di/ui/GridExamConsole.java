469c8b01f92b0a71d1512166aaf26588afbb226d
parkchunsu <pcc0105@gmail.com>
2023-04-16 11:11:50 +0900

    2023.04.16

---
+++ src/main/java/DIstudyspring/di/ui/GridExamConsole.java
@@ -0,0 +1,28 @@
+package DIstudyspring.di.ui;
+
+import DIstudyspring.di.entity.*;
+
+public class GridExamConsole implements ExamConsole {
+	private Exam exam;
+
+	public GridExamConsole() {
+	}
+
+	public GridExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("┌──────────┬──────────┐");
+		System.out.println("│   total  │    avg   │");
+		System.out.println("├──────────┼──────────┤");
+		System.out.printf("│   %3d    │   %3.2f   │\n", exam.total(), exam.avg());
+		System.out.println("└──────────┴──────────┘");
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		this.exam = exam;
+	}
+}
